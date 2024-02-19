30a374a8fe066612e95cf0625bc93227f13283c1
Manikandan Prakash <140643626+M95kandan@users.noreply.github.com>
2024-02-19 18:23:14 +0530

    Create ec2_resource.tf

---
+++ ec2_resource.tf
@@ -0,0 +1,26 @@
+
+resource "aws_instance" "backend" {
+  ami           = "ami-0449c34f967dbf18a"
+  instance_type = "t2.micro"
+  count         = 2
+  key_name      = "xpb_key"
+  vpc_security_group_ids = [aws_security_group.xpb_sg.id]
+
+  tags = {
+    Name   = "xpayback"
+    depart = "backend"
+  }
+}
+
+resource "aws_instance" "haproxy" {
+  ami           = "ami-0449c34f967dbf18a"
+  instance_type = "t2.micro"
+  count         = 1
+  key_name      = "xpb_key"
+  vpc_security_group_ids = [aws_security_group.xpb_sg.id]
+
+  tags = {
+    Name   = "xpayback"
+    depart = "haproxy"
+  }
+}
